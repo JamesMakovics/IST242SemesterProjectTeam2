13978588ffaf58e7028c534690d8c405c5e66d62
Cryptus36 <79118835+Cryptus36@users.noreply.github.com>
2021-02-15 18:54:32 -0500

    added Get Countries and CountriesGuicasedata

---
+++ Covid19CaseTracking/src/CountryGUIcasedata.java
@@ -0,0 +1,9 @@
+
+public class CountryGUIcasedata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Covid19CaseTracking/src/GetCountries.java
@@ -0,0 +1,9 @@
+
+public class GetCountries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
